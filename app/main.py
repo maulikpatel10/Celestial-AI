a6319ba758c81ff794ef0f3108cc9c8f048756a4
maulik10 <maulikpatel0010@gmail.com>
2026-04-18 11:25:44 +0530

    Auto-bind to environment PORT

--- app/main.py
+++ app/main.py
@@ -9,3 +9,9 @@ def root():
 
 app.include_router(chat.router)
 app.include_router(upload.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
